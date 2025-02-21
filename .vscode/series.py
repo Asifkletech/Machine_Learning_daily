f737d683225998aa7c5a44cef8543a3128fc1f08
Asifkhan musafir <151855456+Asifkletech@users.noreply.github.com>
2025-02-21 23:53:40 +0530

    Create series.py

---
+++ .vscode/series.py
@@ -0,0 +1,20 @@
+# Function to print first half in ascending order
+# and second half in descending order
+def printOrder(arr, n):
+    # Sorting the array
+    arr.sort()
+
+    # Printing first half in ascending order
+    for i in range(n // 2):
+        print(arr[i], end=" ")
+
+    # Printing second half in descending order
+    for j in range(n - 1, n // 2 - 1, -1):
+        print(arr[j], end=" ")
+
+# Driver code
+arr = [5, 4, 6, 2, 1, 3, 8, 9, 7]
+n = len(arr)
+
+printOrder(arr, n)
+#
